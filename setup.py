d1dcbaba31bf1420f2fa6d85ebd881d56670390c
Apurv Hajare <apurv@karboncard.com>
2024-08-18 20:56:32 +0530

    remove setup, bump version manually

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="eventbridge_client",
-    version="0.1",
+    name="eventbridge-client",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "boto3",
