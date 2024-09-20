73e9709017bdba86a429d4675def3d9fb4370abf
Bitbucket Pipeline <noreply@bitbucket.org>
2024-09-20 10:35:27 +0000

    Update version in setup.py to v0.4.25 [skip ci]

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="eventbridge-client",
-    version="0.4.24",
+    version="0.4.25",
     packages=find_packages(),
     install_requires=[
         "boto3",
